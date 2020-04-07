ddc6c57c46dcae4dcc34425906bd5c742fd0dba5
Your Name <you@example.com>
2020-04-07 17:16:37 +0600

    first commit

---
+++ table_filter/application/filters.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+import django_filters
+
+class UserFilter(django_filters.FilterSet):
+    first_name = django_filters.CharFilter(lookup_expr='icontains')
+    username = django_filters.CharFilter(lookup_expr='icontains')
+    last_name = django_filters.CharFilter(lookup_expr='icontains')
+    year_joined = django_filters.NumberFilter(field_name='date_joined', lookup_expr='year')
+    year_joined__gt = django_filters.NumberFilter(field_name='date_joined', lookup_expr='year__gt')
+    year_joined__lt = django_filters.NumberFilter(field_name='date_joined', lookup_expr='year__lt')
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', ]
+        ordering = ['id']
---
+++ table_filter/application/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+from .filters import UserFilter
+
+# Create your views here.
+def index(request):
+	return render(request, 'application/index.html')
+
+def search(request):
+    if dict(request.GET):
+    	if 'page' not in request.GET:
+    	    request.session['request_save'] = request.GET
+    else:
+    	request.session['request_save'] = None
+
+    user_list = User.objects.all().order_by('id')
+    page = request.GET.get('page', 1)
+    print(page)
+    if page == 1:
+    	user_filter = UserFilter(request.GET, queryset=user_list)
+    else:
+    	print(dict(request.session['request_save']))
+    	print(dict(request.GET))
+    	user_filter = UserFilter(request.session['request_save'], queryset=user_list)
+    paginator = Paginator(user_filter.qs, 5)
+    try:
+        users = paginator.page(page)
+    except PageNotAnInteger:
+        users = paginator.page(1)
+    except EmptyPage:
+        users = paginator.page(paginator.num_pages)
+
+    return render(request, 'application/index.html', {'filter': user_filter,
+    	'users': users})
